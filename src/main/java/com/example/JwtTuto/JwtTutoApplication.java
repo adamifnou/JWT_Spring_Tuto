e2eb20b861b3696c82893f9f3394e0b0a1da7229
Adam <40129364+adamifnou@users.noreply.github.com>
2024-09-19 16:25:26 +0200

    Initialized Security

---
+++ src/main/java/com/example/JwtTuto/JwtTutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JwtTuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutoApplication.class, args);
+	}
+
+}
